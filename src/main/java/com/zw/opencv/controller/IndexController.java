aca579674817f31c1a23fd6e8736de592d4ba47d
zhucebuchenggong <123456zw>
2019-05-14 09:57:51 +0800

    初步修改

--- src/main/java/com/zw/opencv/controller/IndexController.java
+++ src/main/java/com/zw/opencv/controller/IndexController.java
@@ -25,28 +25,6 @@ public class IndexController {
      */
     @GetMapping("index")
     public R login(){
-
-        System.load("/usr/local/share/openCV/java/libopencv_java320.dylib");
-
-        //以灰度图像的方式读取图像.
-        Mat src = Imgcodecs.imread("/Users/zhangwei/Downloads/test.png", Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
-        src.g(0,0);
-        src.create(0,0,1);
-        src.nativeObj
-//        new ShowImage(src);
-        Mat dst = new Mat();
-
-        //对一个数组应用一个自适应阈值。
-//            该函数根据公式将灰度图像转换为二进制图像：
-        Imgproc.adaptiveThreshold(src, dst, 200,
-                Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 7, 8);
-
-//        new ShowImage(dst);
-
-        Map<String, Object> map = new HashMap<>();
-
-
-
-        return R.ok(map);
+        return null;
     }
 }
---
+++ src/main/java/com/zw/opencv/filedemo/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.zw.opencv.filedemo.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
